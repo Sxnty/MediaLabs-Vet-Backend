adfdc9cbce786a1e5ee343fdb184be3857d04dce
Sxnty <sxntycsgo@gmail.com>
2023-06-04 21:41:39 -0300

    feat routing

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import express from 'express';
-
+import veterinaryRouter from './routes/veterinarys.routes.js'
 const app = express();
+app.use(express.json())
 
 const PORT = 3000;
-app.listen(PORT);
- console.log('hola')
+app.listen(PORT,  console.log('server running on port ', PORT));
+
+app.use('/api',veterinaryRouter)
