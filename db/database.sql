841ca60ebde5142d97adc703ca2101c28fbbe249
Sxnty <sxntycsgo@gmail.com>
2023-06-07 19:45:21 -0300

    few modifications in db

--- db/database.sql
+++ db/database.sql
@@ -4,22 +4,25 @@ use medialabs_dev;
 
 CREATE TABLE veterinary (
     veterinary_owner_ci INT(8) NOT NULL PRIMARY KEY,
-    veterinary_nam VARCHAR(50) NOT NULL,
+    veterinary_name VARCHAR(50) NOT NULL,
     veterinary_password VARCHAR(16) NOT NULL,
     veterinary_email VARCHAR(50) NOT NULL,
-    veterinary_owner_name VARCHAR(25) NOT NULL
+    veterinary_owner_name VARCHAR(25) NOT NULL,
+    veterinary_status BOOLEAN NOT NULL,
+    veterinary_phone INT(12) NOT NULL
 );
 
 CREATE TABLE patients (
     patient_id SERIAL NOT NULL PRIMARY KEY,
-    patient_owner_ci INT(8) NOT NULL,
+    veterinary_id INT(8) NOT NULL,
     patient_nam VARCHAR(25) NOT NULL,
     patient_breed VARCHAR(25) DEFAULT NULL,
-    patient_weight int(5) DEFAULT NULL,
+    patient_weight INT(5) DEFAULT NULL,
     patient_type VARCHAR(15) NOT NULL,
     patient_gender VARCHAR(20) NOT NULL,
     patient_age INT(2) DEFAULT NULL,
-    patient_previous VARCHAR(250) DEFAULT NULL
+    patient_previous VARCHAR(250) DEFAULT NULL,
+    FOREIGN KEY (veterinary_id) REFERENCES veterinary(veterinary_owner_ci)
 );
 
 CREATE TABLE clients (
@@ -27,7 +30,9 @@ CREATE TABLE clients (
     client_name VARCHAR(50) NOT NULL,
     client_phone INT(15) NOT NULL,
     client_address VARCHAR(150) DEFAULT NULL,
-    client_email VARCHAR(50) NOT NULL
+    client_email VARCHAR(50) NOT NULL,
+    veterinary_id INT(8),
+    FOREIGN KEY (veterinary_id) REFERENCES veterinary(veterinary_owner_ci)
 );
 
 CREATE TABLE client_patient (
@@ -35,14 +40,18 @@ CREATE TABLE client_patient (
     client_ci INT(8) NOT NULL,
     PRIMARY KEY(patient_id, client_ci),
     FOREIGN KEY (patient_id) REFERENCES patients(patient_id),
-    FOREIGN KEY (client_ci) REFERENCES clients(client_ci)
+    FOREIGN KEY (client_ci) REFERENCES clients(client_ci),
+    veterinary_id INT(8),
+    FOREIGN KEY (veterinary_id) REFERENCES veterinary(veterinary_owner_ci)
 );
 
 CREATE TABLE veterinary_worker (
     worker_ci INT(8) NOT NULL PRIMARY KEY,
     worker_phone INT(15) NOT NULL,
     worker_name VARCHAR(50) NOT NULL,
-    worker_email VARCHAR(50) NOT NULL
+    worker_email VARCHAR(50) NOT NULL,
+    veterinary_id INT(8),
+    FOREIGN KEY (veterinary_id) REFERENCES veterinary(veterinary_owner_ci)
 );
 
 CREATE TABLE appointment (
@@ -52,5 +61,7 @@ CREATE TABLE appointment (
     worker_ci INT(8) NOT NULL,
     patient_id SERIAL NOT NULL,
     FOREIGN KEY (worker_ci) REFERENCES veterinary_worker(worker_ci),
-    FOREIGN KEY (patient_id) REFERENCES patients(patient_id)
+    FOREIGN KEY (patient_id) REFERENCES patients(patient_id),
+    veterinary_id INT(8),
+    FOREIGN KEY (veterinary_id) REFERENCES veterinary(veterinary_owner_ci)
 );
